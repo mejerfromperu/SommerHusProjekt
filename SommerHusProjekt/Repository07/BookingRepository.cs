8aaa29c64b243198c8cc187169e1acad7605c88f
Isak Gaardsted Møller <ism001@edu.zealand.dk>
2024-05-03 09:39:42 +0200

    Booking repo done igen

--- SommerHusProjekt/Repository07/BookingRepository.cs
+++ SommerHusProjekt/Repository07/BookingRepository.cs
@@ -15,16 +15,13 @@ namespace SommerHusProjekt.Repository07
             SqlConnection connection = new SqlConnection(Secret.GetConnectionString);
             connection.Open();
 
-            string insertSql = "INSERT INTO SommerBooking (StreetName, HouseNumber, Floor, PostalCode, Description, Price, Picture) VALUES (@StreetName, @HouseNumber, @Floor, @PostalCode, @Description, @Price, @Picture)";
+            string insertSql = "INSERT INTO SommerBooking (UserId, SummerHouseId, StartDate, EndDate) VALUES (@UserId, @SummerHouseId, @StartDate, @EndDate)";
 
             SqlCommand cmd = new SqlCommand(insertSql, connection);
-            cmd.Parameters.AddWithValue("@StreetName", b.StreetName);
-            cmd.Parameters.AddWithValue("@HouseNumber", b.HouseNumber);
-            cmd.Parameters.AddWithValue("@Floor", b.Floor);
-            cmd.Parameters.AddWithValue("@PostalCode", b.PostalCode);
-            cmd.Parameters.AddWithValue("@Description", b.Description);
-            cmd.Parameters.AddWithValue("@Price", b.Price);
-            cmd.Parameters.AddWithValue("@Picture", b.Picture);
+            cmd.Parameters.AddWithValue("@UserId", b.UserId);
+            cmd.Parameters.AddWithValue("@SummerHouseId", b.SummerHouseId);
+            cmd.Parameters.AddWithValue("@StartDate", b.StartDate);
+            cmd.Parameters.AddWithValue("@EndDate", b.EndDate);
 
             int rowsAffected = cmd.ExecuteNonQuery();
             Console.WriteLine("Rows affected: " + rowsAffected);
@@ -78,13 +75,11 @@ namespace SommerHusProjekt.Repository07
             Booking b = new Booking();
 
             b.Id = reader.GetInt32(0);
-            b.StreetName = reader.GetString(1);
-            b.HouseNumber = reader.GetString(2);
-            b.Floor = reader.GetString(3);
-            b.PostalCode = reader.GetInt32(4);
-            b.Description = reader.GetString(5);
-            b.Price = reader.GetDecimal(6);
-            b.Picture = reader.GetString(7);
+            b.UserId = reader.GetInt32(1);
+            b.SummerHouseId = reader.GetInt32(2);
+            b.StartDate = reader.GetDateTime(3);
+            b.EndDate = reader.GetDateTime(4);
+
 
 
 
@@ -110,13 +105,11 @@ namespace SommerHusProjekt.Repository07
                 b = new Booking
                 {
                     Id = Convert.ToInt32(reader["Id"]),
-                    StreetName = reader["StreetName"].ToString(),
-                    HouseNumber = reader["HouseNumber"].ToString(),
-                    Floor = reader["Floor"].ToString(),
-                    PostalCode = Convert.ToInt32(reader["PostalCode"]),
-                    Description = reader["Description"].ToString(),
-                    Price = Convert.ToDecimal(reader["Price"]),
-                    Picture = reader["Picture"].ToString(),
+                    UserId = Convert.ToInt32(reader["UserId"]),
+                    SummerHouseId = Convert.ToInt32(reader["SummerHouseId"]),
+                    StartDate = Convert.ToDateTime(reader["StartDate"]),
+                    EndDate = Convert.ToDateTime(reader["EndDate"])
+
                 };
             }
 
@@ -131,16 +124,13 @@ namespace SommerHusProjekt.Repository07
             SqlConnection connection = new SqlConnection(Secret.GetConnectionString);
             connection.Open();
 
-            string updateSql = "UPDATE SommerBooking SET StreetName = @StreetName, HouseNumber = @HouseNumber, Floor = @Floor, Postalcode = @Postalcode, Description = @Description, Price = @Price, Picture = @Picture  WHERE Id = @Id";
+            string updateSql = "UPDATE SommerBooking SET UserId = @UserId, SummerHouseId = @SummerHouseId, StartDate = @StartDate, EndDate = @EndDate  WHERE Id = @Id";
 
             SqlCommand cmd = new SqlCommand(updateSql, connection);
-            cmd.Parameters.AddWithValue("@StreetName", b.StreetName);
-            cmd.Parameters.AddWithValue("@HouseNumber", b.HouseNumber);
-            cmd.Parameters.AddWithValue("@Floor", b.Floor);
-            cmd.Parameters.AddWithValue("@PostalCode", b.PostalCode);
-            cmd.Parameters.AddWithValue("@Description", b.Description);
-            cmd.Parameters.AddWithValue("@Price", b.Price);
-            cmd.Parameters.AddWithValue("@Picture", b.Picture);
+            cmd.Parameters.AddWithValue("@UserId", b.UserId);
+            cmd.Parameters.AddWithValue("@SummerHouseId", b.SummerHouseId);
+            cmd.Parameters.AddWithValue("@StartDate", b.StartDate);
+            cmd.Parameters.AddWithValue("@EndDate", b.EndDate);
             cmd.Parameters.AddWithValue("@Id", id);
 
             int rowsAffected = cmd.ExecuteNonQuery();
