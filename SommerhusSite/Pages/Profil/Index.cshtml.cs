a76248b7c8d210abbacd39031956d220b3c199f2
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2024-05-16 11:37:15 +0200

    UserStory 8

--- SommerhusSite/Pages/Profil/Index.cshtml.cs
+++ SommerhusSite/Pages/Profil/Index.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SommerHusProjekt.Repository07;
+using SommerHusProjekt.Model07;
+using System.ComponentModel.DataAnnotations;
 
 namespace SommerhusSite.Pages.Profil
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+       
+
     }
 }
