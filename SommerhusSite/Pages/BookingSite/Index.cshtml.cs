197ceadde1960b25aeeec5c4122ed5cd74e0452a
mejerfromperu <christianmejer7@gmail.com>
2024-05-20 12:39:36 +0200

    nothing

--- SommerhusSite/Pages/BookingSite/Index.cshtml.cs
+++ SommerhusSite/Pages/BookingSite/Index.cshtml.cs
@@ -20,7 +20,7 @@ namespace SommerhusSite.Pages.BookingSite
 
 
         
-
+        
 
         public IndexModel(ISummerHouseRepository summerlist, IBookingRepository bookinglist)
         {
--- SommerhusSite/Services/SessionHelper.cs
+++ SommerhusSite/Services/SessionHelper.cs
@@ -3,29 +3,29 @@
 namespace SommerhusSite.Services
 {
     public static class SessionHelper
-{
-    public static T Get<T>(HttpContext context)
     {
-        string sessionName = typeof(T).Name;
-        string s = context.Session.GetString(sessionName);
-        if (string.IsNullOrEmpty(s))
+        public static T Get<T>(HttpContext context)
         {
-            throw new ArgumentException($"No session data found for {sessionName}");
+            string sessionName = typeof(T).Name;
+            string s = context.Session.GetString(sessionName);
+            if (string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentException($"No session data found for {sessionName}");
+            }
+            return JsonSerializer.Deserialize<T>(s);
         }
-        return JsonSerializer.Deserialize<T>(s);
-    }
 
-    public static void Set<T>(T data, HttpContext context)
-    {
-        string sessionName = typeof(T).Name;
-        string s = JsonSerializer.Serialize(data);
-        context.Session.SetString(sessionName, s);
-    }
+        public static void Set<T>(T data, HttpContext context)
+        {
+            string sessionName = typeof(T).Name;
+            string s = JsonSerializer.Serialize(data);
+            context.Session.SetString(sessionName, s);
+        }
 
-    public static void Clear<T>(HttpContext context)
-    {
-        string sessionName = typeof(T).Name;
-        context.Session.Remove(sessionName);
+        public static void Clear<T>(HttpContext context)
+        {
+            string sessionName = typeof(T).Name;
+            context.Session.Remove(sessionName);
+        }
     }
-}
 }
