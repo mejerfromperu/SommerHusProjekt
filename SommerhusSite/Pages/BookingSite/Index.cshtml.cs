07f16d829f2c7c6b35f2e0ed91fe0df072b586c3
mejerfromperu <christianmejer7@gmail.com>
2024-05-20 12:53:48 +0200

    dates validtion

--- SommerhusSite/Pages/BookingSite/Index.cshtml.cs
+++ SommerhusSite/Pages/BookingSite/Index.cshtml.cs
@@ -47,9 +47,11 @@ namespace SommerhusSite.Pages.BookingSite
             SummerHouse chosenHouse = SessionHelper.Get<SummerHouse>(HttpContext);
 
             // Validate booking dates
-            if (Booking.StartDate < chosenHouse.DateFrom || Booking.EndDate > chosenHouse.DateTo)
+            if (Booking.StartDate < chosenHouse.DateFrom ||
+    Booking.EndDate > chosenHouse.DateTo ||
+    Booking.EndDate < Booking.StartDate)
             {
-                ModelState.AddModelError("", "Sorry, the booking dates are not within the available period for the selected summer house.");
+                ModelState.AddModelError("", "Sorry, the booking dates are not within the available period for the selected summer house or the end date is before the start date.");
                 return Page();
             }
 
