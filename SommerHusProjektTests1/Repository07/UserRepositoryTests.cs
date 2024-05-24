fce8d1542a1501c3596723a262191d1b85d6d1cc
mejerfromperu <christianmejer7@gmail.com>
2024-05-24 11:54:19 +0200

    small fix

--- SommerHusProjektTests1/Repository07/UserRepositoryTests.cs
+++ SommerHusProjektTests1/Repository07/UserRepositoryTests.cs
@@ -4,6 +4,7 @@ using SommerHusProjekt.Repository07;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,6 +41,22 @@ namespace SommerHusProjekt.Repository07.Tests
             Assert.AreEqual(numberOfUserBefore + 1, _userRepository.GetAll().Count);
         }
 
+        public void AddTestWithId()
+        {
+            // ARange
+            int numberOfUserBefore = _userRepository.GetAll().Count;
+            User newUser = new User("alex", "alex", "88625364", "Alex-UnitTest@gmail.com", "44988232", "streetname", "2", 4000, false, false);
+
+
+            // Act
+            _userRepository.Add(newUser); // tilføjer bruger til database
+
+
+            // Assert 
+            // Der er blevet tilføjet en user så derfor skulle start antallet + 1 gerne været = med antallet are user i databasen
+            Assert.AreEqual(numberOfUserBefore + 1, _userRepository.GetAll().Count);
+        }
+
         [TestMethod]
         [ExpectedException(typeof(SqlException))]
         public void AddUser_DuplicateEmailTest()
@@ -57,7 +74,26 @@ namespace SommerHusProjekt.Repository07.Tests
             Assert.Fail();
         }
 
-        
+        [TestMethod]
+        [ExpectedException(typeof(SqlException))]
+        public void AddUser_DuplicateEmailTest2()
+        {
+            // Arange
+            User newuser = new User("alex", "alex", "88625364", "UNIT-TEST2@gmail.com", "password123", "streetname", "2", "1", 4000, false, false);
+
+            User newuser2 = new User("John", "John", "231423532", "ShouldBeAdded@gmail.com", "password", "streetname", "2", "1", 4000, false, false);
+
+
+            // Act
+            _userRepository.Add(newuser); // Første skulle gerne tilføjes i database 
+            _userRepository.Add(newuser2); // Newuser skal gerne addeds 
+            _userRepository.Add(newuser); // Nummer 2 skal fejle begrund af duplicate email i database.
+
+
+            //Assert
+            Assert.Fail(); // meningen er at vi stadig skal smide en exception efter de 2 første objecter af typen user bliver tilføjet til repo.
+        }
+
 
         [TestMethod()]
         public void DeleteTest()
